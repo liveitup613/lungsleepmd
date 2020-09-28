dfa46a7f524f0f13247bf0d5f031031ba40eafe4
unknown <liveitup613@outlook.com>
2020-09-29 07:10:48 +0800

    update

--- application/views/fe/home.php
+++ application/views/fe/home.php
@@ -70,7 +70,7 @@
                                             <div class="et_pb_column et_pb_column_1_2 et_pb_column_1  et_pb_css_mix_blend_mode_passthrough">
                                                 <div class="et_pb_module et_pb_image et_pb_image_0">
                                                     <span class="et_pb_image_wrap ">
-                                                        <img src="<?php echo base_url('assets/frontend/images/medicus196.jpg');?>" alt="" title="">
+                                                        <img src="<?php echo base_url('assets/frontend/images/medicus196.jpeg');?>" alt="" title="">
                                                     </span>
                                                 </div>
                                             </div> <!-- .et_pb_column -->
